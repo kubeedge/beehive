28a52856387a178ac8701e7a52fd28ea4fd597bb
kadisi <iamkadisi@163.com>
2019-06-29 21:38:53 +0800

    add the apache 2 license
    Signed-off-by: kadisi <iamkadisi@163.com>
    Signed-off-by: zhangjie <iamkadisi@163.com>

--- docs.go
+++ docs.go
@@ -1,18 +1,20 @@
 /*
-#  #############################################
-#  Copyright (c) 2019-2039 All rights reserved.
-#  #############################################
-#
-#  Name:  docs.go
-#  Date:  2019-06-27 15:09
-#  Author:   zhangjie
-#  Email:   iamzhangjie0619@163.com
-#  Desc:
-#
+Copyright 2019 The KubeEdge Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 
+// Package beehive is
 package beehive
 
 //go:generate  protoc  -I=. -I=$GOPATH/src -I=$GOPATH/src/github.com/gogo/protobuf/protobuf   --gogo_out=Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types:./ ./pkg/core/model/message.proto
-
-
